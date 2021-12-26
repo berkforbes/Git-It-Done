f7288b12a5cf4a2c2479340997c3c10aac7dedd3
berkforbes <berkforbes@yahoo.com>
2021-12-26 14:53:33 -0700

    fetch api username, submit button event listener, insert repos into displayRepos function

--- Assets/homepage.js
+++ Assets/homepage.js
@@ -1,11 +1,35 @@
+
+var userFormEl = document.querySelector('#user-form')
+var nameInputEl = document.querySelector('#username')
+
+// Function to take username input and send to getUserRepos function. 
+var formSubmitHandler = function(event){
+event.preventDefault();
+var username = nameInputEl.value.trim();
+
+if (username) {
+    getUserRepos(username);
+    nameInputEl.value="";  //Clears form
+} else {
+    alert('Please enter a github username');
+}
+}
+
+// fetch github API, take username input in form and concatenate as user in api url
 var getUserRepos = function (user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log(data);
+    fetch(apiUrl).then(function(response){ //convert response into json then store as data
+        response.json().then(function(data) {
+            displayRepos(data, user);
         });
     });
 };
 
-getUserRepos("berkforbes");
+// Event listener for sumbit button
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+  };
